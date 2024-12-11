39a25d282c62496548706d911f39a4f0bb7403c7
vladgibeskul <wgv96300@gmail.com>
2024-12-11 09:45:39 +0200

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{/// <summary>
+/// Дан признак транспортного средства: a – автомобиль, в – велосипед, м - мотоцикл, с – самолет, п – поезд. Вывести на экран максимальную скорость транспортного средства в зависимости от введенного признака.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую букву:");
+            char letter = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            switch (letter)
+            {
+                case 'а':
+                    Console.WriteLine("максимальную скорость автомобилья-532,93 км/ч");
+                    break;
+                case 'в':
+                    Console.WriteLine("максимальную скорость велосипеда- 268,8 километра в час");
+                    break;
+                case 'м':
+                    Console.WriteLine("максимальную скорость мотоцикла- 370км/ч");
+                    break;
+                case 'с':
+                    Console.WriteLine("максимальную скорость самолета- 7 274 км/ч");
+                    break;
+                case 'п':
+                    Console.WriteLine("максимальную скорость поезда- 603 километра в час");
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная буква");
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp12/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ConsoleApp12")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("58a1a5b0-edb7-4dc6-8982-15bbe8774be5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
